506413002d32f3c41ef912d096dc4df9bc87c34e
Liana Principe <69564111+lap309@users.noreply.github.com>
2020-09-23 14:08:26 -0400

    Update Full_code.R

--- Full_code.R
+++ Full_code.R
@@ -1,14 +1,20 @@
-##create the dataset that we will be using to create the graphs
+##Download these packages for the code below
 library(dplyr)
-library(tidyverse)
-house<- read.csv("~/Downloads/household_power_consumption.txt, sep=";"")
+library(tidyr)
+library(lubridate)
+
+##Download the dataset and filter to create the dataset we will use with the specific dates. Two columns are added which will be helpful for graphing
+house<- read.csv("~/Downloads/household_power_consumption.txt", sep=";")
 house<- house %>% mutate(new_date=dmy(house$Date))
 use<- housedate %>% filter(new_date=="2007-02-01" | new_date=="2007-02-02")
-use$Global_active_power<- as.numeric(use$Global_active_power)
+house2881<-house[69517:69527, ]
+use<-rbind(use,house2881)
 use<- use %>% mutate(Day = ifelse(new_date=="2007-02-01", "Thursday", ifelse(new_date=="2007-02-02", "Friday", "Saturday")))
 
+##Change the class type of certain columns into date/time class
+use$Global_active_power<- as.numeric(use$Global_active_power)
 use<- use %>% mutate( time=strptime(Time, "%T"))
-use<- paste(use$Date, use$Time)
+use$datetime<- paste(use$Date, use$Time)
 use$datetime<- dmy_hms(use$datetime)
 
 ##create the histogram with red fillings, a new title and a new x-axis label
@@ -16,11 +22,12 @@ g1<- hist(use$Global_active_power, col="red", main="Global Active Power", xlab="
 dev.copy(png, file="plot1.png")
 dev.off()
 
+##Create the second plot, specifying a line graph and edit the labels on the x and y axis
 g2<- plot(use$datetime, use$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.copy(png, file="plot2.png")
 dev.off()
 
-##Creating the 3rd graph          
+##Creating the 3rd graph, change some columns to numeric class. Create a plot that independently will graph 3 different columns, in different colors 
 use$Sub_metering_1<- as.numeric(use$Sub_metering_1)
 use$Sub_metering_2<- as.numeric(use$Sub_metering_2)
 use$Sub_metering_3<- as.numeric(use$Sub_metering_3)
@@ -39,7 +46,7 @@ use$Voltage<- as.numeric(use$Voltage)
 use$Global_reactive_power<- as.numeric(use$Global_reactive_power)
 
 
-##Create a grip of plots
+##Create a grid of plots, and then plot each graph in its designated position
 par(mfcol=c(2,2))
 g2<- plot(use$datetime, use$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 g3<- plot(use$datetime, use$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
