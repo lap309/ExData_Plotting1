5b7c092bebf7a88b692e616ccb5ee0f7bd59b662
Liana Principe <69564111+lap309@users.noreply.github.com>
2020-09-23 14:16:42 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,20 +1,31 @@
-##Download the data
+##Load the packages and the data into R
 library(dplyr)
-house<- read.csv("~/Downloads/household_power_consumption.txt, sep=";"")
+library(tidyr)
+library(lubridate)
+
+house<- read.csv("~/Downloads/household_power_consumption.txt", sep=";")
 house<- house %>% mutate(new_date=dmy(house$Date))
 use<- housedate %>% filter(new_date=="2007-02-01" | new_date=="2007-02-02")
-use$Global_active_power<- as.numeric(use$Global_active_power)
+house2881<-house[69517:69527, ]
+use<-rbind(use,house2881)
 use<- use %>% mutate(Day = ifelse(new_date=="2007-02-01", "Thursday", ifelse(new_date=="2007-02-02", "Friday", "Saturday")))
 
+
+##Change the class of these columns. Most need to be changed to a numeric class
+use$Global_active_power<- as.numeric(use$Global_active_power)
 use<- use %>% mutate( time=strptime(Time, "%T"))
-use<- paste(use$Date, use$Time)
+use$datetime<- paste(use$Date, use$Time)
 use$datetime<- dmy_hms(use$datetime)
+use$Sub_metering_1<- as.numeric(use$Sub_metering_1)
+use$Sub_metering_2<- as.numeric(use$Sub_metering_2)
+use$Sub_metering_3<- as.numeric(use$Sub_metering_3)
+use$Voltage<- as.numeric(use$Voltage)
+use$Global_reactive_power<- as.numeric(use$Global_reactive_power)
 
-
-##Create the layout for multiple plots
+##Create a grid of plots
 par(mfcol=c(2,2))
 
-##Plot the 4 graphs in their respective places
+##Plots 1,2,3,4
 g2<- plot(use$datetime, use$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 g3<- plot(use$datetime, use$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
 points(y=use$Sub_metering_1, x=use$datetime, col="black", type="l", cex=1)
