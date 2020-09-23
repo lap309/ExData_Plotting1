0cb3fe08392e4699da6fd7da83c74d7203502627
Liana Principe <69564111+lap309@users.noreply.github.com>
2020-09-23 14:14:41 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,21 +1,27 @@
-##Creating the data set and changing the classes of the variables to numeric
+##Load the packages and the date, organize the data to represent the dates we want to graph
 library(dplyr)
-house<- read.csv("~/Downloads/household_power_consumption.txt, sep=";"")
+library(tidyr)
+library(lubridate)
+
+house<- read.csv("~/Downloads/household_power_consumption.txt", sep=";")
 house<- house %>% mutate(new_date=dmy(house$Date))
 use<- housedate %>% filter(new_date=="2007-02-01" | new_date=="2007-02-02")
-use$Global_active_power<- as.numeric(use$Global_active_power)
+house2881<-house[69517:69527, ]
+use<-rbind(use,house2881)
 use<- use %>% mutate(Day = ifelse(new_date=="2007-02-01", "Thursday", ifelse(new_date=="2007-02-02", "Friday", "Saturday")))
 
-use<- use %>% mutate( time=strptime(Time, "%T"))
-use<- paste(use$Date, use$Time)
-use$datetime<- dmy_hms(use$datetime)
 
+##Change several columns to numeric class so they can be graphed. Also, create a datetime class of the date so we can see the trend over time
+use$Global_active_power<- as.numeric(use$Global_active_power)
+use<- use %>% mutate( time=strptime(Time, "%T"))
+use$datetime<- paste(use$Date, use$Time)
+use$datetime<- dmy_hms(use$datetime)
 use$Sub_metering_1<- as.numeric(use$Sub_metering_1)
 use$Sub_metering_2<- as.numeric(use$Sub_metering_2)
 use$Sub_metering_3<- as.numeric(use$Sub_metering_3)
 
-##Creating the third graph
-##this originally creates an empty plot, and then we create points for each sub meter (1,2,3) in different colors to understand the relationship
+
+##Create the third plot. First create an empyy plot, then specify the points from the sub metering columns. Graph them with different colors so you can differentiate. Also, add a legend so we cna see which lines are which solumn
 g3<- plot(use$datetime, use$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
 points(y=use$Sub_metering_1, x=use$datetime, col="black", type="l")
 points(y=use$Sub_metering_2, x=use$datetime, col="red", type="l")
@@ -24,3 +30,4 @@ legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"
 
 dev.copy(png,file="plot3.png")
 dev.off()
+
